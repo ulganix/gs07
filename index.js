f3a7e72a0a41e274114c012decfeb207f7a9d9af
uganix <ulganix@outlook.com>
2024-11-30 19:57:53 +0800

    Updates

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-import * as GaussianSplats3D from '@mkkellogg/gaussian-splats-3d';
+document.getElementById("add_to_me")
+                .innerHTML +=
+                `<h3>
+                    This is the text which has
+                    been inserted by JS
+                </h3>`;
 
-const viewer = new GaussianSplats3D.Viewer({
-	    'cameraUp': [0, -1, -0.6],
-	    'initialCameraPosition': [-1, -4, 6],
-	    'initialCameraLookAt': [0, 4, 0]
-});
-viewer.addSplatScene('./peo.ply', {
-	    'splatAlphaRemovalThreshold': 5,
-	    'showLoadingUI': true,
-	    'position': [0, 1, 0],
-	    'rotation': [0, 0, 0, 1],
-	    'scale': [1.5, 1.5, 1.5]
-})
-.then(() => {
-	    viewer.start();
-});
